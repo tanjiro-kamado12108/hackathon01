d15122ecff9f29d16ace69a7d0ac1fbc9f112884
tanjiro-kamado12108 <sandeep.mishra12109@gmail.com>
2025-09-18 05:15:35 +0000

    asd

--- app.py
+++ app.py
@@ -357,9 +357,13 @@ def delete_note(note_id):
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username = request.form['username']
+        username_or_email = request.form['username']
         password = request.form['password']
-        user = User.query.filter_by(username=username).first()
+        # Try to find user by username or email
+        user = User.query.filter((User.username == username_or_email)).first()
+        if not user:
+            # Try again for email-style usernames
+            user = User.query.filter((User.username == username_or_email)).first()
         if user and check_password_hash(user.password, password):
             session['user_id'] = user.id
             session['role'] = user.role
